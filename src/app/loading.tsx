50c945cfc316f013c0982ae29b379fd8c923a643
efe <eefee1987@gmail.com>
2024-05-15 07:34:57 +0200

    add loading & error page

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function loading(): JSX.Element {
+  return (
+    <div className='mt-20 text-center opacity-50 h-screen'>cargando...</div>
+  );
+}
+
+export default loading;
