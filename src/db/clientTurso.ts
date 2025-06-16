70bf1b6f3112781be6434bcf94a89be12ab1ea45
efe <efe13dev@gmail.com>
2025-06-16 07:35:17 +0200

    fix: downgrade @libsql/client y ajustar formato de comillas en clientTurso.ts

--- src/db/clientTurso.ts
+++ src/db/clientTurso.ts
@@ -1,23 +1,21 @@
-import { createClient } from '@libsql/client';
+import { createClient } from "@libsql/client";
 
 const client = createClient({
-  url: process.env.TURSO_DATABASE_URL ?? '',
-  authToken: process.env.TURSO_AUTH_TOKEN
+  url: process.env.TURSO_DATABASE_URL ?? "",
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
 export async function getAdoptions(): Promise<any> {
   const availableAdoptions = await client.execute(
-    'SELECT * FROM animals WHERE adopted = 0 ORDER BY register_date DESC'
+    "SELECT * FROM animals WHERE adopted = 0 ORDER BY register_date DESC"
   );
-
   return availableAdoptions;
 }
 
 export async function getOneAdoption(id: string): Promise<any> {
   const adoption = await client.execute({
-    sql: 'SELECT * FROM animals WHERE id = ?',
-    args: [id]
+    sql: "SELECT * FROM animals WHERE id = ?",
+    args: [id],
   });
-
   return adoption;
 }
